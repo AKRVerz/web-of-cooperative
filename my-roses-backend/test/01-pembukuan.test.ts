27947be87fb3380022f612fea244ae7dd0f6fe6a
AKRVerz <kurniawansyahapri@gmail.com>
2023-04-29 12:28:50 +0700

    test_progress

--- my-roses-backend/test/01-pembukuan.test.ts
+++ my-roses-backend/test/01-pembukuan.test.ts
@@ -1,30 +1,98 @@
+import moment from 'moment';
+import { Express } from 'express';
+import supertest from 'supertest';
 import * as middleware from '../src/middleware/pembukuans';
+import * as userMiddleware from '../src/middleware/users';
+import { USER_ROLE } from '../src/utils/constant';
+import { hashText } from '../src/utils/encryption';
 import { extractMiddleware, resolveMiddleware } from './utils/common';
-import { createMockData, mockNextFn } from './utils/mock';
+import {
+  createMockApi,
+  createMockData,
+  createPostRequest,
+  mockLoginMw,
+  mockNextFn,
+} from './utils/mock';
+import _ from 'lodash';
 
 describe('Pembukuan Middleware', () => {
-  it('Can validate user', async () => {
-    const mw = extractMiddleware(middleware.createPembukuanMw);
+  let app: Express;
 
-    const { req, res } = createMockData({
-      url: '/pembukuans',
-      method: 'POST',
-      body: {
-        tanggal: new Date().toISOString(),
-        uraian: 'Testing',
-        sumWood: 500.6,
-        harga: 2000,
-        masuk: 50,
-        keluar: 10,
-        jumlah: 2000,
-        afterCashBack: 200,
-        sumCashBack: 100,
-        cashBack: 1000,
-      },
+  beforeAll(async () => {
+    app = await createMockApi();
+  });
+
+  it('get data', async () => {
+    const id = 0;
+
+    await supertest(app).get('/users').expect(true);
+  });
+
+  it('get user', async () => {
+    const res = await createPostRequest(app).send({
+      id: 0,
     });
 
-    await resolveMiddleware({ req, res, mw });
-
-    expect(req.pembukuan.id).toBeDefined();
+    expect(res.statusCode).toBe(true);
   });
+
+  // it.skip('get user', async () => {
+  //   const mwUser = extractMiddleware(userMiddleware.getUsersMw);
+
+  //   await createPostRequest(app).send({
+  //     email: 'admin@admin.com',
+  //   });
+  // });
+
+  // it.skip("Can't create a data without an admin authorization", async () => {
+  //   const mw = extractMiddleware(middleware.createPembukuanMw);
+
+  //   const { req, res } = createMockData({
+  //     url: '/pembukuans',
+  //     method: 'POST',
+  //     body: {
+  //       tanggal: new Date().toISOString(),
+  //       uraian: 'Testing',
+  //       sumWood: 500.6,
+  //       harga: 2000,
+  //       masuk: 50,
+  //       keluar: 10,
+  //       jumlah: 2000,
+  //       afterCashBack: 200,
+  //       sumCashBack: 100,
+  //       cashBack: 1000,
+  //     },
+  //   });
+
+  //   await mw(req, res, mockNextFn);
+
+  //   expect(res.statusCode).toBe(401);
+  // });
+
+  // it('Can login with correct credentials', async () => {
+  //   const { req, res } = createMockData({
+  //     url: '/auth/login',
+  //     method: 'POST',
+  //     body: {
+  //       email: 'admin@admin.com',
+  //       password: await hashText('admin'),
+  //       role: USER_ROLE.ADMIN,
+  //       noKtp: '100401',
+  //       alamat: 'Jl Hi Rais',
+  //       tanggal: moment('1997-07-16').toISOString() as unknown as Date,
+  //     },
+  //   });
+
+  //   req.isAdmin = true;
+
+  // const mw = extractMiddleware(userMiddleware.loginMw);
+
+  // await resolveMiddleware({ req, res, mw });
+
+  // //   // await mw(req, res, mockNextFn);
+
+  // const { data } = res._getJSONData();
+
+  // expect(data.token).toBeTruthy();
+  // });
 });
--- my-roses-backend/test/utils/mock.ts
+++ my-roses-backend/test/utils/mock.ts
@@ -1,15 +1,70 @@
+import { config as dotenvConfig } from 'dotenv';
+import httpMocks from 'node-mocks-http';
+import * as userMiddleware from '../../src/middleware/users';
+import { extractMiddleware, resolveMiddleware } from './common';
 import express, { Express } from 'express';
 import supertest from 'supertest';
+import root from '../../src/utils/root';
+
+dotenvConfig();
 
 export const createMockApi = async () => {
   const app = express();
 
+  root(app);
+
   return app;
 };
 
-export const createPostRequest = (app: Express, endpoint = '/auth') => {
+export const createPostRequest = (app: Express, endpoint = '/user') => {
   return supertest(app)
-    .post(endpoint)
+    .get(endpoint)
     .set('Content-Type', 'application/json')
     .set('Accept', 'application/json');
+};
+
+export const createMockData = ({
+  url,
+  method,
+  body = {},
+  query = {},
+}: {
+  method: httpMocks.RequestMethod;
+  url: string;
+  body?: httpMocks.Body;
+  query?: httpMocks.Query;
+}) => {
+  const { req, res } = httpMocks.createMocks({
+    url,
+    method,
+    query,
+    body,
+    headers: {
+      'content-type': 'application/json',
+      accept: 'application/json',
+    },
+  });
+
+  return { req, res };
+};
+
+export const mockNextFn = () => {
+  return null;
+};
+
+export const mockLoginMw = async () => {
+  const { req, res } = createMockData({
+    url: '/auth/login',
+    method: 'POST',
+    body: {
+      email: 'admin@admin.com',
+      password: 'admin',
+    },
+  });
+
+  const mw = extractMiddleware(userMiddleware.loginMw);
+
+  await mw(req, res, mockNextFn);
+
+  return { req, res };
 };
